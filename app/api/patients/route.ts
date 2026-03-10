45e067918d5fe557af1f5aedf48e3eba04061de2
Juliana Negreiros <juliananegreiros@Julianas-MacBook-Pro.local>
2026-03-10 20:31:32 -0300

    feat: add post patients

--- app/api/patients/route.ts
+++ app/api/patients/route.ts
@@ -1,8 +1,16 @@
 import { NextResponse } from 'next/server'
-import { listPatients } from '@/app/modules/patients/patients.repo'
+import { listPatients, createPatient } from '@/app/modules/patients/patients.repo'
 
 export async function GET() {
   const patients = await listPatients()
 
   return NextResponse.json({ data: patients }, { status: 200 })
+}
+
+export async function POST(request: Request) {
+  const body = await request.json()
+  const { name, species } = body
+  const patient = await createPatient({ name, species })
+
+  return NextResponse.json({ data: patient }, { status: 201 })
 }
--- app/modules/patients/patients.repo.ts
+++ app/modules/patients/patients.repo.ts
@@ -4,8 +4,24 @@ import { PrismaPg } from '@prisma/adapter-pg'
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 const prisma = new PrismaClient({ adapter })
 
+type Patient = {
+  name: string
+  species: string
+}
+
 export async function listPatients() {
   const patients = await prisma.patient.findMany()
+
+  return patients
+}
+
+export async function createPatient(patient: Patient) {
+  const patients = await prisma.patient.create({
+    data: {
+      name: patient.name,
+      species: patient.species,
+    },
+  })
 
   return patients
 }
